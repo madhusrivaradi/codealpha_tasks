6449e5aee32f7a8462fb8d9878c87c7a39393e1f
madhusrivaradi <madhusrivaradi363@gmail.com>
2026-05-15 11:25:00 +0530

    Update chatbot.py

--- CodeAlpha_chatbot/chatbot.py
+++ CodeAlpha_chatbot/chatbot.py
@@ -1,3 +1,4 @@
+import time
 def chatbot():
     print("I'm ready to chat")
     while True:
@@ -5,11 +6,15 @@ def chatbot():
         if user == "hello":
             print("chatbot:Hi")
         elif user == "how are you":
-
             print("chatbot:I'am fine,thanks")
+        elif "python"in user:
+            print("chatbot:python is a power full language")
+        elif "time"in user:
+            current_time=time.ctime()
+            print("bot:",current_time)
         elif user =="bye":
             print("chatbot:Goodbye!")
             break
         else:
             print("sorry i dont understand")
-chatbot() 
+chatbot()
